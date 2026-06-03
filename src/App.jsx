def1b8476f9b79ed76915ecdfe1cd33f7a351b28
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-06-03 11:28:04 +1000

    feat: fetch legislator data from live api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { useStoreState } from 'easy-peasy';
+import { useState, useEffect } from 'react'
+import { useStoreState, useStoreActions } from 'easy-peasy';
 import { FaFacebook, FaYoutube, FaTwitter, FaInstagram, FaTiktok } from 'react-icons/fa'
 import { FaArrowRotateLeft } from 'react-icons/fa6';
 import { MdLink, MdPhone, MdEmail } from 'react-icons/md'
@@ -10,18 +10,28 @@ import MembersGrid from './components/MembersGrid';
 import filterLegislators from './utils/filterLegislators';
 import useFilters from './hooks/useFilters';
 
-function App() {
+export default function App() {
   const [isFilterVisible, setIsFilterVisible] = useState(false);
-  const legislators = useStoreState(state => state.legislators);
   const filters = useFilters();
+  const fetchLegislatorsAPI = useStoreActions(action => action.fetchLegislatorsAPI);
+  const isLoading = useStoreState(state => state.isLoading);
+  const loadingError = useStoreState(state => state.loadingError);
+  const legislators = useStoreState(state => state.legislatorsAPI);
+
+  useEffect(() => {
+    fetchLegislatorsAPI();
+  }, []);
 
   return (
     <div className='h-screen flex flex-col'>
       <Header setIsFilterVisible={setIsFilterVisible} />
-
       <div className='bg-pink-300 flex-1 flex flex-col sm:flex-row sm:overflow-hidden'>
         <FilterPanel isFilterVisible={isFilterVisible} setIsFilterVisible={setIsFilterVisible} />
-        <MembersGrid legislators={filterLegislators(legislators, filters)} />
+        {isLoading && <p className='text-4xl mx-1'>Loading...</p>}
+        {loadingError && <p className='text-4xl mx-1'>{loadingError.message}</p>}
+        {!isLoading && !loadingError &&
+          <MembersGrid legislators={filterLegislators(legislators, filters)} />
+        }
       </div>
 
       <Footer />
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, action } from 'easy-peasy'
+import { createStore, action, thunk } from 'easy-peasy'
 import legislators from '../data/legislators-current.json';
 
 export default createStore({
@@ -23,5 +23,42 @@ export default createStore({
     partyFilter: { Democrat: true, Republican: true, Independent: true },
     setPartyFilter: action((state, payload) => {
         state.partyFilter = payload;
+    }),
+
+    legislatorsAPI: [],
+    isLoading: false,
+    loadingError: null,
+    setLegislatorsAPI: action((state, payload) => {
+        state.legislatorsAPI = payload;
+    }),
+    setIsLoading: action((state, payload) => {
+        state.isLoading = payload;
+    }),
+    setLoadingError: action((state, payload) => {
+        state.loadingError = payload;
+    }),
+    fetchLegislatorsAPI: thunk(async (actions) => {
+        actions.setIsLoading(true);
+        actions.setLoadingError(null);
+        try {
+            //will throw TypeError: Failed to fetch
+            const res = await fetch('https://unitedstates.github.io/congress-legislators/legislators-current.json');
+            //will throw ResponseCode: ###
+            if (!res.ok) throw new Error(`ResponseCode: ${res.status}`);
+            const json = await res.json();
+            actions.setLegislatorsAPI(json);
+            // await new Promise(resolve =>
+            //     setTimeout(() => {
+            //         actions.setLegislatorsAPI(legislators);
+            //         resolve();
+            //     }, 3000)
+            // );
+        }
+        catch (err) {
+            actions.setLoadingError(err);
+        }
+        finally {
+            actions.setIsLoading(false);
+        }
     }),
 })
